9c84b00c295aba551d36359b8866c77c69c698a4
1643697 <55006299+1643697@users.noreply.github.com>
2019-12-16 10:49:14 -0500

    trial

---
+++ Sketchysearch.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sektch</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="Style.css">
+    </head>
+    <body>
+        
+        <h1>Search Bar</h1>
+        
+        <!-- <script src="Javascripts/lettersOnly.js"></script> -->
+        
+        <form name = "webinfo" action="action.php" method="POST">
+        <!-- This form posts to	action.php,	and	the	page	
+        action.php can access the posted form data using	
+        the	names of the input elements (“name”	and	“age”)
+        like so: $_POST[‘name’]	and	$_POST[‘age’]-->
+        
+            <br>First Name:<br>
+            <input name="firsts" id="firsts" placeholder="firsts" onkeyup="lettersOnly(this)">
+            
+            <input class="button" type="submit" value="Submit"> 
+        </form>
+
+        <br>
+
+        
+    </body>
+</html>
---
+++ action.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title> Confirmation</title>
+        <link rel="stylesheet" type="text/css" href="style/mainStyle.css"> <!--external css-->
+    </head>
+
+    <body>
+        <div class="center">
+
+             Hi <?php echo htmlspecialchars($_POST['firstname']); ?> ! 
+            
+            <?php 
+                //login credentials to db
+                $servername = "localhost";
+                $username = "php";
+                $password = "vm";
+                $dbname = "userinfo";
+                //store form inputs to variables
+                $firsts = htmlspecialchars($_POST['firsts']);
+                $lasts = $_SERVER['REMOTE_ADDR']
+            ?>
+
+            <?php
+                //connection to db
+                $conn = mysqli_connect($servername, $username, $password, $dbname);
+                //insert data to db
+                $sql = "INSERT INTO users (firsts, lasts)
+                    VALUES ('{$firsts}', '{$lasts}')";
+                 //confirmation
+                 
+                 if (mysqli_query($conn, $sql)) {
+                    echo "Thank you for submitting your application.";
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+                //close connection 
+                mysqli_close($conn);
+            ?>
+        </div>
+    </body>
+</html>
